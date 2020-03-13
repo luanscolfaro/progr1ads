9c8dd749797dbc47e181ba77a686ec6d067ce496
luanscolfaro <61217037+luanscolfaro@users.noreply.github.com>
2020-03-13 19:30:56 -0300

    Delete Q8.py

--- Q8.py
+++
@@ -1,8 +0,0 @@
-# Luan Scolfaro Amorim Carneiro
-# UNIFIP - Patos
-# 05 de março de 2020
-# Questão 08 - Escreva um programa que leia um valor inteiro e calcule o seu cubo.
-
-n = int(input('Digite um numero:'))
-t = n*3
-print('O triplo de {} vale {}.'.format(n, t))
