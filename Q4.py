31be74d2f1c1b5005b97f707bca3acfb68965f86
luanscolfaro <61217037+luanscolfaro@users.noreply.github.com>
2020-03-13 19:30:23 -0300

    Delete Q4.py

--- Q4.py
+++
@@ -1,8 +0,0 @@
-# Luan Scolfaro Amorim Carneiro
-# UNIFIP - Patos
-# 05 de março de 2020
-# Questão 04 - Escreva um programa que leia uma temperatura em graus Fahrenheit, transforme-a em graus Celsius e exiba o resultado.
-
-f = float(input('Informe a temperatura em °F:'))
-c = ((f-32)/18000)
-print('A temperatura de {} °F corresponde a {} °C!'.format(f,c))
