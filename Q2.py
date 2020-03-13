87c15e76d72f73024258324ef0bad821750b8c8c
luanscolfaro <61217037+luanscolfaro@users.noreply.github.com>
2020-03-13 19:01:05 -0300

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,3 +1,10 @@
+# Luan Scolfaro Amorim Carneiro
+# UNIFIP - Patos
+# 05 de março de 2020
+# Questão 02 - Em um banho de 5 minutos, fechando o registro ao se ensaboar,
+# são gastos 45 litros de água. Sabendo que em 1 m3 de água há 1.000 litros,
+# calcule quantos banhos de 5 minutos são necessários para consumir 1 metro cúbico de água?
+
 tempo_banho = int(input("Quanto tempo demora seu banho? "))
 litros_minutos = 9
 gasto_no_banho = tempo_banho * litros_minutos
